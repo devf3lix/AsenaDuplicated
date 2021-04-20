3383972e8b2a59705ef1ce5f91f6c4ba2b5a5cc4
#DevFelix <82602282+asenablack@users.noreply.github.com>
2021-04-20 18:47:19 +0300

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -41,15 +41,48 @@ Asena.addCommand({pattern: 'tagall ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC}
 const rp = "*Report!*"
 const ss = "Adminleri Etiketler"
 Asena.addCommand({pattern: 'tagadmin', fromMe: true, desc: ss}, (async (message, match) => {
-
-    const getGroupAdmins = async (participants) => {
-        var admins = [];
-        msg = '';
-        for (let i of participants) {
-            if (i.isAdmin) {
-                admins.push(i.id.replace('c.us', 'a.whatsapp.net'));
-            }
+    let grup = await message.client.groupMetadata(message.jid);
+    var jids = [];
+    mesaj = '';
+    grup['participants'].map(async (uye) => {
+        if (uye.isAdmin) {
+            mesaj += '@' + uye.id.split('@')[0] + ' ';
+            jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
         }
-        await message.client.sendMessage(message.jid,rp, MessageType.extendedText, {contextInfo: {mentionedJid: admins}, previewType: 0})
+    });
+    await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+}));
+
+const tor = "Adminlere rapor gönderir."
+const rep = "*Lütfen Rapor Edilecek Kullanıcının Mesajına Yanıt Verin!*"
+const rap = "=== ```Report``` ===\n\n"
+Asena.addCommand({pattern: 'report ?(.*)', fromMe: true, desc: tor}, (async (message, match) => {
+    if (match[1] == '' && message.reply_message) {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,rap + '*Kullanıcı:* ' + '@' + message.reply_message.jid.split('@')[0] , MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+        
+    }
+    else if (match[1] !== '' && message.reply_message) {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,rap + '*Kullanıcı:* ' + '@' + message.reply_message.jid.split('@')[0] + `\n*Sebep:* ${match[1]}`, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }
+    else if (!message.reply_message) {
+        return message.client.sendMessage(message.jid,rep, MessageType.text);
     }
 }));
