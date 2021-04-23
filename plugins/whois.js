59dd8db62599e08a59632978e4acb4fd150be6fa
#DevFelix <82602282+asenablack@users.noreply.github.com>
2021-04-23 05:17:42 +0300

    Update whois.js

--- plugins/whois.js
+++ plugins/whois.js
@@ -44,102 +44,235 @@ Asena.addCommand({ pattern: 'whois', fromMe: true, desc: das }, async (message, 
 const lhs = "Yanıtlanan mesaji loga kaydeder!"
 const rep = "*Lütfen Herhangi Bir Mesajı Yanıtlayın!*"
 const lgg = "```===== [LOGGED MESSAGE] =====```\n\n"
+const gr = " ID'li Gruptan "
 const ks = "Numaralı Kişiden\n"
 const msh = "*Mesaj:* \n\n"
 Asena.addCommand({ pattern: 'log', fromMe: true, desc: lhs}, (async (message, match) => { 
 
-    if (!message.reply_message) {
-        return await message.client.sendMessage(
-            message.jid,
-            rep,
-            MessageType.text
-        );
-    }
-    else if (message.reply_message.text) {
-        await message.client.sendMessage(
-            message.client.user.jid,
-            lgg + 'wa.me' + message.reply_message.jid.split('@')[0] + ' ' + ks + msh + message.reply_message.text,
-            MessageType.text
-        );
-    }
-    else if (message.reply_message.image) {
-        var location = await message.client.downloadAndSaveMediaMessage({
-            key: {
-                remoteJid: message.reply_message.jid,
-                id: message.reply_message.id
-            },
-            message: message.reply_message.data.quotedMessage
-        });
-        ffmpeg(location)
-        .save('log.jpg')
-        .on('end', async () => {
-            await message.client.sendMessage(
-                message.client.user.jid,
-                fs.readFileSync('log.jpg'),
-                MessageType.image,
-                { caption: lgg + 'wa.me' + message.reply_message.jid.split('@')[0] + ' ' + ks }
-            );
-        });
-    }
-    else if (message.reply_message.video) {
-        var location = await message.client.downloadAndSaveMediaMessage({
-            key: {
-                remoteJid: message.reply_message.jid,
-                id: message.reply_message.id
-            },
-            message: message.reply_message.data.quotedMessage
-        });
-        ffmpeg(location)
-        .save('log.mp4')
-        .on('end', async () => {
-            await message.client.sendMessage(
-                message.client.user.jid,
-                fs.readFileSync('log.mp4'),
-                MessageType.video,
-                { mimetype: Mimetype.mpeg, caption: lgg + 'wa.me' + message.reply_message.jid.split('@')[0] + ' ' + ks }
-            );
-        });
-    }
-    else if (message.reply_message.audio) {
-        var location = await message.client.downloadAndSaveMediaMessage({
-            key: {
-                remoteJid: message.reply_message.jid,
-                id: message.reply_message.id
-            },
-            message: message.reply_message.data.quotedMessage
-        });
-        ffmpeg(location)
-        .save('log.mp3')
-        .on('end', async () => {
-            await message.client.sendMessage(
-                message.client.user.jid,
-                fs.readFileSync('log.mp3'),
-                MessageType.audio,
-                { mimetype: Mimetype.mp4Audio, caption: lgg + 'wa.me/' + message.reply_message.jid.split('@')[0] + ' ' + ks }
-            );
-        });
-    }
-    else {
-        var location = await message.client.downloadAndSaveMediaMessage({
-            key: {
-                remoteJid: message.reply_message.jid,
-                id: message.reply_message.id
-            },
-            message: message.reply_message.data.quotedMessage
-        });
-        ffmpeg(location)
-        .save('log.webp')
-        .on('end', async () => {
-            await message.client.sendMessage(
-                message.client.user.jid,
-                fs.readFileSync('log.webp'),
-                MessageType.sticker
-            );
-            await message.client.sendMessage(
-                message.client.user.jid,
-                lgg + 'wa.me/' + message.reply_message.jid.split('@')[0] + ' ' + ks,
+    const meta = await message.client.groupMetadata(message.jid)
+    const usmeta = message.client.isOnWhatsApp(message.jid)
+
+    if (message.jid.includes('-')) {
+        if (!message.reply_message) {
+            return await message.client.sendMessage(
+                message.jid,
+                rep,
                 MessageType.text
             );
-        });
+        }
+        else if (message.reply_message.text) {
+            await message.client.sendMessage(
+                message.client.user.jid,
+                lgg + meta.id + gr + 'wa.me/' + message.reply_message.jid.split('@')[0] + ' ' + ks + msh + message.reply_message.text,
+                MessageType.text
+            );
+        }  
+        else if (message.reply_message.image) {
+            var location = await message.client.downloadAndSaveMediaMessage({
+                key: {
+                    remoteJid: message.reply_message.jid,
+                    id: message.reply_message.id
+                },
+                message: message.reply_message.data.quotedMessage
+            });
+            ffmpeg(location)
+            .save('log.jpg')
+            .on('end', async () => {
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    fs.readFileSync('log.jpg'),
+                    MessageType.image,
+                    { caption: lgg + meta.id + gr + 'wa.me/' + message.reply_message.jid.split('@')[0] + ' ' + ks }
+                );
+            });
+        }
+        else if (message.reply_message.video) {
+            var location = await message.client.downloadAndSaveMediaMessage({
+                key: {
+                    remoteJid: message.reply_message.jid,
+                    id: message.reply_message.id
+                },
+                message: message.reply_message.data.quotedMessage
+            });
+            ffmpeg(location)
+            .save('log.mp4')
+            .on('end', async () => {
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    fs.readFileSync('log.mp4'),
+                    MessageType.video,
+                    { mimetype: Mimetype.mpeg, caption: lgg + meta.id + gr + 'wa.me/' + message.reply_message.jid.split('@')[0] + ' ' + ks }
+                );
+            });
+        }
+        else if (message.reply_message.audio) {
+            var location = await message.client.downloadAndSaveMediaMessage({
+                key: {
+                    remoteJid: message.reply_message.jid,
+                    id: message.reply_message.id
+                },
+                message: message.reply_message.data.quotedMessage
+            });
+            ffmpeg(location)
+            .save('log.mp3')
+            .on('end', async () => {
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    fs.readFileSync('log.mp3'),
+                    MessageType.audio,
+                    { mimetype: Mimetype.mp4Audio} 
+                );
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    lgg + meta.id + gr + 'wa.me/' + message.reply_message.jid.split('@')[0] + ' ' + ks,
+                    MessageType.text
+                );
+            });
+        }
+        else {
+            var location = await message.client.downloadAndSaveMediaMessage({
+                key: {
+                    remoteJid: message.reply_message.jid,
+                    id: message.reply_message.id
+                },
+                message: message.reply_message.data.quotedMessage
+            });
+            ffmpeg(location)
+            .save('log.webp')
+            .on('end', async () => {
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    fs.readFileSync('log.webp'),
+                    MessageType.sticker
+                );
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    lgg + meta.id + gr + 'wa.me/' + message.reply_message.jid.split('@')[0] + ' ' + ks,
+                    MessageType.text
+                );
+            });
+        }
+    }
+    else if (!message.jid.includes('-')) {
+        if (!message.reply_message) {
+            return await message.client.sendMessage(
+                message.jid,
+                rep,
+                MessageType.text
+            );
+        }
+        else if (message.reply_message.text) {
+            await message.client.sendMessage(
+                message.client.user.jid,
+                lgg + 'wa.me/' + message.reply_message.jid + ' ' + ks + msh + message.reply_message.text,
+                MessageType.text
+            );
+        }  
+        else if (message.reply_message.image) {
+            var location = await message.client.downloadAndSaveMediaMessage({
+                key: {
+                    remoteJid: message.reply_message.jid,
+                    id: message.reply_message.id
+                },
+                message: message.reply_message.data.quotedMessage
+            });
+            ffmpeg(location)
+            .save('log.jpg')
+            .on('end', async () => {
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    fs.readFileSync('log.jpg'),
+                    MessageType.image,
+                    { caption: lgg + 'wa.me/' + message.reply_message.jid.split('@')[0] + ' ' + ks }
+                );
+            });
+        }
+        else if (message.reply_message.video) {
+            var location = await message.client.downloadAndSaveMediaMessage({
+                key: {
+                    remoteJid: message.reply_message.jid,
+                    id: message.reply_message.id
+                },
+                message: message.reply_message.data.quotedMessage
+            });
+            ffmpeg(location)
+            .save('log.mp4')
+            .on('end', async () => {
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    fs.readFileSync('log.mp4'),
+                    MessageType.video,
+                    { mimetype: Mimetype.mpeg, caption: lgg + 'wa.me/' + message.reply_message.jid.split('@')[0] + ' ' + ks }
+                );
+            });
+        }
+        else if (message.reply_message.audio) {
+            var location = await message.client.downloadAndSaveMediaMessage({
+                key: {
+                    remoteJid: message.reply_message.jid,
+                    id: message.reply_message.id
+                },
+                message: message.reply_message.data.quotedMessage
+            });
+            ffmpeg(location)
+            .save('log.mp3')
+            .on('end', async () => {
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    fs.readFileSync('log.mp3'),
+                    MessageType.audio
+                );
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    lgg + 'wa.me/' + message.reply_message.jid.split('@')[0] + ' ' + ks,
+                    MessageType.text
+                );
+            });
+        }
+        else {
+            var location = await message.client.downloadAndSaveMediaMessage({
+                key: {
+                    remoteJid: message.reply_message.jid,
+                    id: message.reply_message.id
+                },
+                message: message.reply_message.data.quotedMessage
+            });
+            ffmpeg(location)
+            .save('log.webp')
+            .on('end', async () => {
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    fs.readFileSync('log.webp'),
+                    MessageType.sticker
+                );
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    lgg + 'wa.me/' + message.reply_message.jid.split('@')[0] + ' ' + ks,
+                    MessageType.text
+                );
+            });
+        }
     }
 }));
+
+const scan = "Girilen numaranın WhatApp'ta kayıtlı olup olmadığını kontrol eder."
+const nos = "*Lütfen Herhangi Bir Telefon Numarası Gir!*\n*Örnek:* ```.scan 90xxxx```"
+const fin = " *Numaralı Kişi WhatApp Kullanmıyor! ❌*"
+
+Asena.addCommand({ pattern: 'scan ?(.*)', fromMe: true, desc: scan}, (async (message, match) => { 
+
+    if (match[1] == '') return await message.client.sendMessage(message.jid, nos, MessageType.text);
+
+    var exists = await message.client.isOnWhatsApp(match[1])
+    if (exists) {
+        await message.client.sendMessage(message.jid, '```' + match[1] + '``` *Numaralı Kişi WhatApp Kullanıyor! ✅*\n*JID Adresi:* ' + exists.jid, MessageType.text);
+    }
+    else {
+        await message.client.sendMessage(message.jid,match[1] + fin, MessageType.text);
+    }
+}));
+
+
+
+
+
